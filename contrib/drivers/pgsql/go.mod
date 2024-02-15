060918ec8126f3793d68b03e0dfe77bd97302515
Ghostbb <mucgll0328@gmail.com>
2024-02-15 10:15:01 +0800

    feat(gbhttp): :sparkles: 新增常用功能

--- contrib/drivers/pgsql/go.mod
+++ contrib/drivers/pgsql/go.mod
@@ -3,20 +3,32 @@ module ghostbb.io/gb/contrib/drivers/pgsql
 go 1.22
 
 require (
-	ghostbb.io/gb v1.2.6 // indirect
+	ghostbb.io/gb v1.2.7
+	gorm.io/driver/postgres v1.5.6
+	gorm.io/gorm v1.25.7
+)
+
+require (
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
 	github.com/jackc/pgx/v5 v5.5.3 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	go.opentelemetry.io/otel v1.22.0 // indirect
+	go.opentelemetry.io/otel/metric v1.22.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.22.0 // indirect
+	go.opentelemetry.io/otel/trace v1.22.0 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	gorm.io/driver/postgres v1.5.6 // indirect
-	gorm.io/gorm v1.25.7 // indirect
 )
 
-replace (
-	ghostbb.io/gb => ../../../
-)
+replace ghostbb.io/gb => ../../../
---
+++ net/gb_http/gb_http_server_tools.go
@@ -0,0 +1,60 @@
+package gbhttp
+
+import (
+	"fmt"
+	gbvar "ghostbb.io/gb/container/gb_var"
+	gberror "ghostbb.io/gb/errors/gb_error"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"strings"
+)
+
+// GetBearerToken Get bearer token from header (Authorization: Bearer xxx)
+func GetBearerToken(c *gin.Context) string {
+	var token string
+	auth := c.GetHeader("Authorization")
+	prefix := "Bearer "
+	if auth != "" && strings.HasPrefix(auth, prefix) {
+		token = auth[len(prefix):]
+	}
+	return token
+}
+
+// ParseJSON Parse body json data to struct
+func ParseJSON(c *gin.Context, obj interface{}) error {
+	if err := c.ShouldBindJSON(obj); err != nil {
+		return gberror.New(fmt.Sprintf("Parse request json failed: %s", err.Error()))
+	}
+	return nil
+}
+
+// ParseParam Param returns the value of the URL param
+func ParseParam(c *gin.Context, key string) string {
+	val := c.Param(key)
+	return val
+}
+
+// ParseQuery Parse query parameter to struct
+func ParseQuery(c *gin.Context, obj any) error {
+	if err := c.ShouldBindQuery(obj); err != nil {
+		return gberror.New(fmt.Sprintf("Parse request query failed: %s", err.Error()))
+	}
+	return nil
+}
+
+// ParseForm Parse body form data to struct
+func ParseForm(c *gin.Context, obj interface{}) error {
+	if err := c.ShouldBindWith(obj, binding.Form); err != nil {
+		return gberror.New(fmt.Sprintf("Parse request form failed: %s", err.Error()))
+	}
+	return nil
+}
+
+func Set(c *gin.Context, key string, value any) {
+	c.Set(key, value)
+}
+
+func Get(c *gin.Context, key string) *gbvar.Var {
+	value, _ := c.Get(key)
+	return gbvar.New(value)
+}
